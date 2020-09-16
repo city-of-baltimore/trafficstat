5982712993a217ac00c51c9f9fa35038d1049423
Brian Seel <Brian.Seel@baltimorecity.gov>
2020-09-16 17:08:19 -0400

    Accidentally left commented code

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 """Main driver for the traffic stat scripts"""
 import argparse
 
-from trafficstat.backfill_census_tracts import geocode_acrs, geocode_acrs_sanitized
+from trafficstat.enrich_data import geocode_acrs, geocode_acrs_sanitized, clean_road_names
 from trafficstat.crash_data_ingestor import CrashDataReader
 from trafficstat.ms2generator import WorksheetMaker
 
 parser = argparse.ArgumentParser(description='Traffic Data Parser')
-parser.add_argument('-c', '--censustracts', action='store_true',
-                    help='Adds census tract information to the acrs_roadway and acrs_roadway_sanitized tables')
+parser.add_argument('-e', '--enrich', action='store_true',
+                    help='Adds census tract information and cleans roadway infromation in the roadway tables')
 parser.add_argument('-p', '--parsecrashdata',
                     help='Parse ACRS xml crash data files in the specified directory, and insert them into the '
                          'DOT_DATA database')
@@ -16,9 +16,10 @@ parser.add_argument('-m', '--ms2', action='store_true',
 
 args = parser.parse_args()
 
-if args.censustracts:
+if args.enrich:
     geocode_acrs()
     geocode_acrs_sanitized()
+    clean_road_names()
 
 if args.parsecrashdata:
     cls = CrashDataReader()
