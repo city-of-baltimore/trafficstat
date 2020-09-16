769496c5d620a3e1a13cd4368e282e4712a1e2e1
Brian Seel <Brian.Seel@baltimorecity.gov>
2020-09-16 17:07:31 -0400

    Adding code to clean road names

--- trafficstat/__init__.py
+++ trafficstat/__init__.py
@@ -1,5 +1,5 @@
 """Sets up namespace for the creds to be imported"""
 
-from . import creds, backfill_census_tracts, crash_data_ingestor, ms2generator
+from . import creds, enrich_data, crash_data_ingestor, ms2generator
 
-__all__ = [creds, backfill_census_tracts, crash_data_ingestor, ms2generator]
+__all__ = [creds, enrich_data, crash_data_ingestor, ms2generator]
