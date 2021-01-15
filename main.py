bba1ec88b0ab7d021d1d4fdcea04940a66244aa9
Brian Seel <Brian.Seel@baltimorecity.gov>
2021-01-15 18:10:21 -0500

    Couple bugs

--- main.py
+++ main.py
@@ -1,30 +1,41 @@
 """Main driver for the traffic stat scripts"""
 import argparse
 
-from src.enrich_data import geocode_acrs, geocode_acrs_sanitized, clean_road_names
-from src.crash_data_ingestor import CrashDataReader
-from src.ms2generator import WorksheetMaker
+from src.trafficstat.enrich_data import geocode_acrs, geocode_acrs_sanitized, clean_road_names
+from src.trafficstat.crash_data_ingestor import CrashDataReader
+from src.trafficstat.ms2generator import WorksheetMaker
 
 parser = argparse.ArgumentParser(description='Traffic Data Parser')
-parser.add_argument('-e', '--enrich', action='store_true',
-                    help='Adds census tract information and cleans roadway infromation in the roadway tables')
-parser.add_argument('-p', '--parsecrashdata',
-                    help='Parse ACRS xml crash data files in the specified directory, and insert them into the '
-                         'DOT_DATA database')
-parser.add_argument('-m', '--ms2', action='store_true',
-                    help='Generate CSV files that MS2 uses to import crash data. Pulls from the DOT_DATA database')
+subparsers = parser.add_subparsers(dest='subparser_name', help='sub-command help')
+
+# Enrich
+parser_enrich = subparsers.add_parser('enrich', help='Adds census tract information and cleans roadway infromation in '
+                                                     'the roadway tables')
+
+# Parse
+parser_parse = subparsers.add_parser('parse', help='Parse ACRS xml crash data files in the specified directory, and '
+                                                   'insert them into a database')
+parser_parse.add_argument('-c', '--conn_str', default='sqlite:///crash.db',
+                          help='Custom database connection string (default: sqlite:///crash.db)')
+parser_parse.add_argument('-d', '--directory', help='Directory containing ACRS XML files to parse')
+
+# Generate
+parser_generate = subparsers.add_parser('generate', help='Generate CSV files that MS2 uses to import crash data. Pulls '
+                                                         'from the DOT_DATA database')
+
 
 args = parser.parse_args()
 
-if args.enrich:
+if args.subparser_name == 'enrich':
     geocode_acrs()
     geocode_acrs_sanitized()
     clean_road_names()
 
-if args.parsecrashdata:
-    cls = CrashDataReader(args.parsecrashdata)
+if args.subparser_name == 'parse':
+    cls = CrashDataReader(args.conn_str)
+    cls.read_crash_data(dir_name=args.directory)
 
-if args.ms2:
+if args.subparser_name == 'generate':
     ws_maker = WorksheetMaker()
     with ws_maker:
         ws_maker.add_crash_worksheet()
