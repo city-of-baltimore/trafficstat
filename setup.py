f49058296462331826728e444889155865478b39
Brian Seel <Brian.Seel@baltimorecity.gov>
2021-02-23 12:07:21 -0500

    Updating geocoding requirement

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         'pandas~=1.2.2',
         'pyodbc~=4.0.30',
         'tqdm~=4.57.0',
-        'balt-geocoder @ git+https://github.com/city-of-baltimore/Geocoder@main#egg=balt-geocoder',
+        'balt-geocoder @ git+https://github.com/city-of-baltimore/Geocoder@1.0#egg=balt-geocoder',
         'xlsxwriter~=1.3.7',
     ]
 )
