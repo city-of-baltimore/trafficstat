b827b1219a1e3640e85429e4aea93358374ae7d6
Brian Seel <Brian.Seel@baltimorecity.gov>
2022-03-09 16:56:52 -0500

    The geocoding is broken... this fixes it

--- src/trafficstat/enrich_data.py
+++ src/trafficstat/enrich_data.py
@@ -11,20 +11,16 @@ CRASH_LOCATION (nvarchar(max))
 """
 import argparse
 import re
+import urllib.parse
 from typing import Tuple
-from json.decoder import JSONDecodeError
 
 import requests
-from arcgis.geocoding import reverse_geocode  # type: ignore
-from arcgis.gis import GIS  # type: ignore
 from loguru import logger
 from sqlalchemy import create_engine  # type: ignore
 from sqlalchemy.orm import Session  # type: ignore
 
 from ._merge import insert_or_update
 from .ms2generator_schema import Base, RoadwaySanitized
-
-GIS()
 
 
 class Enrich:
@@ -36,47 +32,6 @@ class Enrich:
 
         with self.engine.begin() as connection:
             Base.metadata.create_all(connection)
-
-    def geocode_acrs_sanitized(self) -> None:
-        """
-        Fills in the CENSUS_TRACT column in acrs_roadway_sanitized
-        :return: None
-        """
-        with Session(self.engine) as session:
-            qry = session.query(RoadwaySanitized.REPORT_NO,
-                                RoadwaySanitized.X_COORDINATES,
-                                RoadwaySanitized.Y_COORDINATES).filter(RoadwaySanitized.CENSUS_TRACT.is_(None))
-
-        for row in qry.all():
-            if not (row[1] and row[2]):
-                # we can't reverse geocode if we dont have lat/long
-                continue
-
-            try:
-                geocode_result = reverse_geocode([row[1], row[2]])
-            except RuntimeError as err:
-                logger.error(f'Runtime error: {err}')
-                continue
-            except JSONDecodeError as err:
-                logger.error(f'Invalid response from geocoder: {err}')
-
-            address = geocode_result.get('address').get('Address')
-            city = geocode_result.get('address').get('City')
-            state = geocode_result.get('address').get('Region')
-            req = requests.get(f'https://geocoding.geo.census.gov/geocoder/geographies/address?'
-                               f'street={address}&city={city}&state={state}&benchmark=Public_AR_Census2020&'
-                               f'vintage=Census2020_Census2020&layers=10&format=json')
-            if not req.json():
-                continue
-
-            if req.json().get('result') and req.json().get('result').get('addressMatches'):
-                insert_or_update(RoadwaySanitized(
-                    REPORT_NO=row[0],
-                    CENSUS_TRACT=req.json().get('result').get('addressMatches')[0].get('geographies').get(
-                        'Census Blocks')[0].get('TRACT'),
-                ), self.engine)
-            else:
-                logger.warning('No census tract for sanitized roadway: {row}', row=row)
 
     def get_cleaned_location(self) -> None:
         """Intelligently analyzes the road_name and reference_road_name to determine the location of the crash"""
@@ -97,30 +52,43 @@ class Enrich:
             # Take care of special cases wth Ft Mchenry tunnel and 295, where we want to just dump them at spots
             road_name = row[1].upper()
             ref_road_name = row[2].upper()
+            tract = None
+            cleaned_location = None
 
             if any(x in y for x in tunnel_names for y in (road_name, ref_road_name)):
-                road_name_clean = '1200 FRANKFURST AVE'
+                road_name_clean = None
                 ref_road_name_clean = None
             elif any(x in y for x in parkway_names for y in (road_name, ref_road_name)):
-                road_name_clean = '2700 WATERVIEW AVE'
+                road_name_clean = None
                 ref_road_name_clean = None
             else:
                 road_name_clean, ref_road_name_clean = self.clean_road_names(row[1], row[2])
 
-            if road_name_clean == ref_road_name_clean:
-                road_name_clean = row[1]
-                ref_road_name_clean = None
+                if road_name_clean == ref_road_name_clean:
+                    road_name_clean = row[1]
+                    ref_road_name_clean = None
 
-            cleaned_location = road_name_clean \
-                if ref_road_name_clean is None \
-                else f'{road_name_clean} & {ref_road_name_clean}'
+                cleaned_location = road_name_clean \
+                    if ref_road_name_clean is None \
+                    else f'{road_name_clean} & {ref_road_name_clean}'
+
+                req = requests.get(f'https://geocoding.geo.census.gov/geocoder/geographies/address?'
+                                   f'street={urllib.parse.quote_plus(cleaned_location)}&city=Baltimore&'
+                                   f'state=MD&benchmark=Public_AR_Current&vintage=Current_Current&layers=10&format=json')
+
+                if req.json().get('result') and req.json().get('result').get('addressMatches'):
+                    tract = req.json().get('result').get('addressMatches')[0].get('geographies').get(
+                            'Census Block Groups')[0].get('TRACT')
+                else:
+                    logger.warning(f'No census tract for sanitized roadway: {row}')
 
             if road_name_clean or ref_road_name_clean:
                 insert_or_update(RoadwaySanitized(
                     ROAD_NAME_CLEAN=road_name_clean,
                     REFERENCE_ROAD_NAME_CLEAN=ref_road_name_clean,
                     CRASH_LOCATION=cleaned_location,
-                    REPORT_NO=row[0]
+                    REPORT_NO=row[0],
+                    CENSUS_TRACT=tract
                 ), self.engine)
 
     def clean_road_names(self, road_name, ref_road_name) -> Tuple[str, str]:
@@ -173,5 +141,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     enricher = Enrich(args.conn_str)
-    enricher.geocode_acrs_sanitized()
     enricher.get_cleaned_location()
--- tests/test_enrich_data.py
+++ tests/test_enrich_data.py
@@ -4,32 +4,6 @@ from sqlalchemy.orm import Session  # type: ignore
 from trafficstat.ms2generator_schema import CrashSanitized, RoadwaySanitized
 
 
-def test_geocode_acrs_sanitized(enrich):
-    """Test for geocode_acrs_sanitized"""
-    with Session(bind=enrich.engine) as session:
-        session.add_all([
-            CrashSanitized(REPORT_NO=11), CrashSanitized(REPORT_NO=12),
-            CrashSanitized(REPORT_NO=13), CrashSanitized(REPORT_NO=14),
-            RoadwaySanitized(REPORT_NO=11, X_COORDINATES=-76.6147012764419, Y_COORDINATES=39.2662592254051),
-            RoadwaySanitized(REPORT_NO=12, X_COORDINATES=-76.6145704472065, Y_COORDINATES=39.3054704684803),
-            RoadwaySanitized(REPORT_NO=13, X_COORDINATES=-76.6316923284531, Y_COORDINATES=39.3069128837032),
-            RoadwaySanitized(REPORT_NO=14, X_COORDINATES=-76.6728773204327, Y_COORDINATES=39.2810172044491)])
-        session.commit()
-
-    enrich.geocode_acrs_sanitized()
-
-    expected = {11: None,
-                12: '110100',
-                13: '140200',
-                14: '200800'}
-
-    with Session(enrich.engine) as session:
-        for report_no, census_tract in expected.items():
-            qry = session.query(RoadwaySanitized.CENSUS_TRACT). \
-                filter(RoadwaySanitized.REPORT_NO.is_(report_no))
-            assert qry.first()[0] == census_tract
-
-
 def test_clean_road_names(enrich):
     """Test for clean_road_names"""
     assert enrich.clean_road_names('1100 NORTH AVE', 'HOWARD ST') == ('NORTH AVE', 'HOWARD ST')
@@ -56,11 +30,11 @@ def test_get_cleaned_location(enrich):
 
     enrich.get_cleaned_location()
 
-    expected = {1: ('2700 WATERVIEW AVE', '2700 WATERVIEW AVE', None),
-                2: ('1200 FRANKFURST AVE', '1200 FRANKFURST AVE', None),
-                3: ('1100 NORTH AVE', '1100 NORTH AVE', None),
-                4: ('GILMORE ST & PRATT ST', 'GILMORE ST', 'PRATT ST'),
-                5: ('1200 FRANKFURST AVE', '1200 FRANKFURST AVE', None),
+    expected = {1: (None, None, None, None),
+                2: (None, None, None, None),
+                3: ('1100 NORTH AVE', '1100 NORTH AVE', None, '130300'),
+                4: ('GILMORE ST & PRATT ST', 'GILMORE ST', 'PRATT ST', '190300'),
+                5: (None, None, None, None),
                 }
 
     with Session(enrich.engine) as session:
@@ -68,8 +42,10 @@ def test_get_cleaned_location(enrich):
 
             qry = session.query(RoadwaySanitized.CRASH_LOCATION,
                                 RoadwaySanitized.ROAD_NAME_CLEAN,
-                                RoadwaySanitized.REFERENCE_ROAD_NAME_CLEAN).\
+                                RoadwaySanitized.REFERENCE_ROAD_NAME_CLEAN,
+                                RoadwaySanitized.CENSUS_TRACT).\
                 filter(RoadwaySanitized.REPORT_NO.is_(report_no))
             assert qry.first()[0] == cleaned[0]
             assert qry.first()[1] == cleaned[1]
             assert qry.first()[2] == cleaned[2]
+            assert qry.first()[3] == cleaned[3]
